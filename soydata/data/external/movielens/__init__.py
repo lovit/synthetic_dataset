9215a1cbd79d6aaf22d675dd2b85b0f86f0091fd
lovit <soy.lovit@gmail.com>
2019-12-10 23:54:42 +0900

    Use download_movielens() instead _movielens_20m(), _movielens_small()

--- soydata/data/external/movielens/__init__.py
+++ soydata/data/external/movielens/__init__.py
@@ -1,3 +1,2 @@
-from .io import download_movielens_20m
-from .io import download_movielens_small
+from .io import download_movielens
 from .io import load_rating
--- soydata/data/external/movielens/io.py
+++ soydata/data/external/movielens/io.py
@@ -15,29 +15,27 @@ movielens_small_readme_url = 'http://files.grouplens.org/datasets/movielens/ml-l
 movielens_20m_dir = f'{external_path}/movielens/ml-20m/'
 movielens_small_dir = f'{external_path}/movielens/ml-latest-small/'
 
-def download_movielens_20m(force=False):
-    data_url = movielens_20m_url
-    readme_url = movielens_20m_readme_url
-    dirname = movielens_20m_dir
-    download(data_url, readme_url, dirname, force)
+def download_movielens(size='20m', force=False):
+    """
+    Arguments
+    ---------
+    size : str
+        Datasize. one of ['20m', 'small']
+    force : Boolean
+        If True, it overwrites data if the file exists
+    """
 
-def download_movielens_small(force=False):
-    data_url = movielens_small_url
-    readme_url = movielens_small_readme_url
-    dirname = movielens_small_dir
+    data_url, readme_url, dirname = check_size(size)
     download(data_url, readme_url, dirname, force)
 
 def download(data_url, readme_url, dirname, force=False):
-    print("This function downloads MovieLens data from GroupLens\n" \
-          f"Please read first {readme_url}\n" \
-          "All permissions are in GroupLens, and this function is an external utility" \
-          " to conventiently use MovieLens data.\n")
-
     if os.path.exists(dirname) and (not force):
         answer = input('The data already is downloaded. Re-download it? [yes|no]').lower().strip()
         if answer != 'yes':
             print('Terminated')
             return None
+
+    notice(readme_url)
 
     filename = data_url.split("/")[-1]
     zippath = f'{external_path}/movielens/{filename}'
@@ -48,8 +46,47 @@ def download(data_url, readme_url, dirname, force=False):
     if os.path.exists(zippath):
         os.remove(zippath)
 
+def notice(readme_url):
+    print("This function downloads MovieLens data from GroupLens\n" \
+          f"Please read first {readme_url}\n" \
+          "All permissions are in GroupLens, and this function is an external utility" \
+          " to conventiently use MovieLens data.\n")
+
+def check_size(size):
+    if not size in ['20m', 'small']:
+        raise ValueError("`size` must be one of ['20m', 'small']")
+
+    if size == '20m':
+        data_url = movielens_20m_url
+        readme_url = movielens_20m_readme_url
+        dirname = movielens_20m_dir
+    else:
+        data_url = movielens_small_url
+        readme_url = movielens_small_readme_url
+        dirname = movielens_small_dir
+    return data_url, readme_url, dirname
+
 def load_rating(size='20m'):
-    dirname = movielens_20m_dir if size == '20m' else movielens_small_dir
+    """
+    Arguments
+    ---------
+    size : str
+        Datasize. one of ['20m', 'small']
+
+    Returns
+    -------
+    user_item : scipy.sparse.csr_matrix
+        (user, item) = rate
+    timestamps : numpy.ndarray
+        UNIX time format datetime
+    """
+    data_url, readme_url, dirname = check_size(size)
+    if not os.path.exists(dirname):
+        print("The data has not been downloaded, Download it first.\n")
+        download(data_url, readme_url, dirname)
+    else:
+        notice(readme_url)
+
     rating_path = f'{dirname}/ratings.csv'
 
     users = []
